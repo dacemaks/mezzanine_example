182c78ac429ae4f41c4d46a4ed3b71144739d271
dacemaks <102698227+dacemaks@users.noreply.github.com>
2023-06-28 13:33:20 +0200

    Update wsgi.py

--- mezzanine_example/wsgi.py
+++ mezzanine_example/wsgi.py
@@ -10,7 +10,7 @@ https://docs.djangoproject.com/en/1.10/howto/deployment/wsgi/
 import os
 
 from django.core.wsgi import get_wsgi_application
-from mezzanine.utils.conf import real_project_name
+from mezzanine.utils.conf import mezzanine
 
 os.environ.setdefault("DJANGO_SETTINGS_MODULE",
                       "%s.settings" % real_project_name("mezzanine_example"))
